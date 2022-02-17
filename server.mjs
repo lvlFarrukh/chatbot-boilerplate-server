013a1f95fcb2de36495eb24d4c1569b180f9cd44
FARRUKH ASLAM <farrukha303@gmail.com>
2022-02-17 07:14:14 -0600

    initial commit

---
+++ server.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import twilio from 'twilio';
+import "dotenv/config";
+import sendMessage from './utiles/whatsappSendMessage.mjs'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+
+app.post("/twiliowebhook", (req, res) => {
+
+    // console.log("req: ", JSON.stringify(req.body));
+
+    console.log("message: ", req.body.Body);
+
+   // TODO: ask dialogflow what to respond
+   
+   
+    let twiml = new twilio.twiml.MessagingResponse()
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.header('Content-Type', 'text/xml');
+    res.send(twiml.toString());
+})
+
+//! Whatsapp webhook
+app.post("/whatsappwebhook", (req, res) => {
+    let message = req.body.Body;
+    let senderID = req.body.From;
+
+    console.log(`${message} --- ${senderID} --- ${process.env.TWILIO_NUMBER}`)
+
+    sendMessage(twilioClient, "Hello From Pc", senderID, process.env.TWILIO_NUMBER)
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
---
+++ utiles/whatsappSendMessage.mjs
@@ -0,0 +1,18 @@
+const sendMessage = async (client, message, senderID, twilioNumber) => {
+    console.log(`${client} -- ${message} -- ${senderID} -- ${twilioNumber}`)
+    try {
+        await client.messages
+            .create({
+                from: twilioNumber,
+                to: senderID,
+                body: message,
+            })
+            .then(message => console.log(message.sid));
+            
+    }   
+    catch (error){
+        console.log(error.message);
+    }
+}
+
+export default sendMessage
